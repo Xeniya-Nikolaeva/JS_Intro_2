7c8cf01e4915d3752c2bf11ef29f56768324f01e
Xeniya-Nikolaeva <143876682+Xeniya-Nikolaeva@users.noreply.github.com>
2024-09-22 18:37:57 +0300

    Update script.js

--- WH2/script.js
+++ WH2/script.js
@@ -1,26 +1,15 @@
-// get div element into another element
-
-
-const correctDiv = document.querySelector("div > div");
-correctDiv.addEventListener("mouseover", () => {
-   correctDiv.style.background = "green"; 
-});
-correctDiv.addEventListener("mouseout", () => {
-    correctDiv.style.background = "red";
-});
-
-/*
-//get all div from document
 const allDiv = document.querySelectorAll("div");
 for (let i = 0; i < allDiv.length; i++)
 {
     let element = allDiv[i];
     element.addEventListener("mouseover", () => {
-        element.style.border = "5px solid black";
+        element.style.border = "border-width: thick; border-style: double;border-color: blueviolet";
+        element.style.backgroundColor = "blue";
+        
     });
     element.addEventListener("mouseout", () => {
-        element.style.border = "0px solid black";
+        element.style.border = "border-width: medium; border-style: double;border-color: blueviolet";
+        element.style.background = "#15be0f";
     });
+    
 }
-
-document.querySelector */
